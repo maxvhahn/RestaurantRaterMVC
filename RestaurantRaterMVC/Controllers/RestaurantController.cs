b852833511556027d0120f82f70edbd42eb96ad2
Max Hahn <max.vhahn@gmail.com>
2021-06-24 13:19:33 -0400

    Details functionality added

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -101,5 +101,21 @@ namespace RestaurantRaterMVC.Controllers
             }
             return View(restaurant);
         }
+
+        //Get: Restaurant/Details/{id}
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
