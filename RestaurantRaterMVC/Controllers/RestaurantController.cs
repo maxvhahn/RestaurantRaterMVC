fd33e169b77711b155bf1faad39495d675f4cb8e
Max Hahn <max.vhahn@gmail.com>
2021-06-24 12:20:58 -0400

    Edit functionality was added

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -63,6 +64,42 @@ namespace RestaurantRaterMVC.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //Get: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a restaurant by that id
+        // If the restaurant doesn't exist 
+        // If the restaurant does exist return the restaurant and view
+
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+            
+        }
+
+        //Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            return View(restaurant);
         }
     }
 }
